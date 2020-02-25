c22ee03f562433fda304b081ba015518ce3e8f85
Barbier <barbier.lucy@gmail.com>
2020-02-25 14:32:18 +0100

    initiated stylesheets

--- app/models/creek.rb
+++ app/models/creek.rb
@@ -2,6 +2,6 @@ class Creek < ApplicationRecord
   belongs_to :user
   has_many :bookings
 
-  validates :title, :country, :city, :rating, :available, :price, :description, :capacity presence: true
+  validates :title, :country, :city, :rating, :available, :price, :description, :capacity, presence: true
   validates :title,  uniqueness: true
 end
